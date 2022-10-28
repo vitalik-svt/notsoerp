7202109f4823ee210c74c89f1d5e6d6d2da27fe0
MrCamembert <zederblatt@gmail.com>
2022-10-28 13:05:17 +0300

    autocreation of superuser with env params

--- erp/config.py
+++ erp/config.py
@@ -1,5 +1,4 @@
 import os
-import json
 from dotenv import load_dotenv
 
 # .env file in root, so get the top up folder
@@ -9,12 +8,21 @@ top_up_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../")
 load_dotenv(os.path.join(top_up_folder, ".env"))
 
 class Config:
+    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    POSTGRES_USER = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+    POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@pgdb:{POSTGRES_PORT}/{POSTGRES_DB}"
+
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB_CONNECTION_STRING')
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv('EMAIL_USER')
-    MAIL_PASSWORD = os.getenv('EMAIL_PASS')
+
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT'))
+    MAIL_USE_TLS = True if os.getenv('MAIL_USE_TLS').lower() == 'true' else False
+
     APP_USER = os.getenv('APP_USER')
     APP_PASSWORD = os.getenv('APP_PASSWORD')
+    APP_USER_MAIL = os.getenv('APP_USER_MAIL')
+    APP_DEBUG_MODE = True if os.getenv('APP_DEBUG_MODE').lower() == 'true' else False
